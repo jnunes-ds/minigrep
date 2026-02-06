e51869975e167ad46f17de261683ba8ee5563a58
Denison Junior <denison.junior@cebraspe.org.br>
2026-02-06 10:38:54 -0300

    feat: add error handling for argument parsing and integrate colored output library

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
-use std::{fs, env};
+use std::{fs, env, process};
+use colored;
+use colored::Colorize;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("{}: {}", "Problem parsing arguments".bold().red(), err.underline().red());
+        process::exit(1);
+    });
+
     println!("searching for {}", config.query);
     println!("In file {}", config.filename);
 
@@ -18,11 +24,14 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config {query, filename}
+        Ok(Config {query, filename})
     }
 
 }
