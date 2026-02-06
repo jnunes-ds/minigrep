ff3f3811e07a34e1f8c576525409ce2428b0d60b
Denison Junior <denison.junior@cebraspe.org.br>
2026-02-06 10:19:35 -0300

    feat: add file reading and display content functionality

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{fs, env};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -7,4 +7,8 @@ fn main() {
     let filename = &args[2];
     println!("searching for {}", query);
     println!("In file {}", filename);
+    
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+    println!("With thext:\n{}", contents);
 }
