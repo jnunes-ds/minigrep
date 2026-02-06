018702af5ac53065b57ffe46f920bbd4a64e99dd
Denison Junior <denison.junior@cebraspe.org.br>
2026-02-06 11:09:00 -0300

    refactor: move `Config` and `run` to a new library module

---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+use std::{error, fs};
+
+pub fn run(config: Config) -> Result<(), Box<dyn error::Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+    println!("With thext:\n{}", contents);
+    Ok(())
+}
+
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config {query, filename})
+    }
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use std::{fs, env, process, error};
+use std::{env, process};
 use colored;
 use colored::Colorize;
+
+use minigrep::{Config, run};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -17,29 +19,5 @@ fn main() {
         println!("{}: {}", "Application error".bold().red(), e.to_string().red());
         process::exit(1);
     };
-}
-
-fn run(config: Config) -> Result<(), Box<dyn error::Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-    println!("With thext:\n{}", contents);
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config {query, filename})
-    }
-
 }
 
