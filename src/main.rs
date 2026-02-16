c5e5a7b1744193f2d3751d01280f9991ac740ef4
Denison Junior <denison.junior@cebraspe.org.br>
2026-02-16 17:56:51 -0300

    refactor: improve `Config` argument parsing and streamline search logic and using iterators for it

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,15 @@ use colored::Colorize;
 use minigrep::{Config, run};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("{}: {}", "Problem parsing arguments".bold().red(), err.red());
         process::exit(1);
     });
 
-    eprintln!("searching for {}", config.query);
-    eprintln!("In file {}", config.filename);
+    eprintln!("searching for: {}", config.query);
+    eprintln!("In file: {}", config.filename);
+    eprintln!("-----------------------------");
 
     if let Err(e) = run(config) {
         eprintln!("{}: {}", "Application error".bold().red(), e.to_string().red());
